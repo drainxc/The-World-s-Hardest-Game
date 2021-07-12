6418104def9d4dcecae064951e2ef8cb90ae960e
ldh7228 <ldh7228@gmail.com>
2021-07-12 09:06:37 +0900

    Add obstacle

--- index.js
+++ index.js
@@ -5,17 +5,15 @@ let leftPressed = false;
 let rightPressed = false;
 let upPressed = false;
 let downPressed = false;
+let bounce = true;
 
 ctx.lineWidth = 5;
 let playerX = 5;
 let playerY = 5;
 let Speed = 7;
-let obstacleX = [];
-let obstacleY = [];
-let obstacle = [];
+let obstacleY = 5;
 
 function drawPlayer() {
-    
     if(playerX < 5) playerX = 5;
     if(playerY < 5) playerY = 5;
     if(playerX > 975) playerX = 975;
@@ -41,6 +39,24 @@ function start(event) {
     }
     move();
 }
+
+setInterval(function(){
+    ctx.clearRect(0, 0, 1000, 500);
+    if (bounce) {
+        if (obstacleY + 235 > 470) {
+            bounce = false;
+        }
+        obstacleY += Speed;
+    }
+    else {
+        if (obstacleY + 235 < 10) {
+            bounce = true;
+        }
+        obstacleY -= Speed;
+    }
+    drawPlayer();
+    drawObstacle();
+}, 20);
 
 function stop(event) {
     if (event.key == 'ArrowLeft' || event.key == 'a') {
@@ -73,17 +89,17 @@ function move() {
         playerY += Speed;
     }
     drawPlayer();
+    drawObstacle();
+}
+
+function drawObstacle() {
+    for (let i = 4; i < 30; i++) {
+        ctx.strokeStyle = 'black';
+        ctx.fillStyle = 'blue';
+        ctx.strokeRect(i * 30, obstacleY + 235, 20, 20);
+        ctx.fillRect(i * 30, obstacleY + 235, 20, 20);
+    }
 }
 
 document.addEventListener('keydown', start);
 document.addEventListener('keyup', stop);
-drawObstacle();
-
-function drawObstacle() {
-    for (let i = 2; i < 30; i++) {
-        ctx.strokeStyle = 'black';
-        ctx.fillStyle = 'blue';
-        ctx.strokeRect(i * 30, 5, 20, 20);
-        ctx.fillRect(i * 30, 5, 20, 20);
-    }
-}
