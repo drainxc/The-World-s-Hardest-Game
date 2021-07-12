826c4f8995e1f4d591b5cac6a19ad51cb5875605
ldh7228 <ldh7228@gmail.com>
2021-07-12 17:24:58 +0900

    Add dead event

--- index.js
+++ index.js
@@ -11,13 +11,22 @@ ctx.lineWidth = 5;
 let playerX = 5;
 let playerY = 5;
 let Speed = 7;
-let obstacleY = 5;
+let obstacleX = [];
+let obstacleY = 235;
 
 function drawPlayer() {
-    if(playerX < 5) playerX = 5;
-    if(playerY < 5) playerY = 5;
-    if(playerX > 975) playerX = 975;
-    if(playerY > 475) playerY = 475; 
+    if (playerX < 3) playerX = 3;
+    if (playerY < 3) playerY = 3;
+    if (playerX > 978) playerX = 978;
+    if (playerY > 478) playerY = 478;
+    console.log(obstacleY);
+    console.log(playerY);
+    for (let i = 0; i < 25; i++) {
+        if ((obstacleX[i] + 20) >= playerX && (obstacleX[i] - 20) <= playerX && obstacleY + 20 >= playerY && obstacleY - 20 <= playerY) {
+            console.log('asdf');
+            location.reload();
+        }
+    }
     ctx.strokeStyle = 'black';
     ctx.fillStyle = 'red';
     ctx.strokeRect(playerX, playerY, 20, 20);
@@ -25,31 +34,31 @@ function drawPlayer() {
 }
 
 function start(event) {
-    if (event.key == 'ArrowLeft' || event.key == 'a') {
+    if (event.key == 'ArrowLeft' || event.key == 'a' || event.key == 'A') {
         leftPressed = true;
     }
-    else if (event.key == 'ArrowRight' || event.key == 'd') {
+    else if (event.key == 'ArrowRight' || event.key == 'd' || event.key == 'D' || event.key == '6') {
         rightPressed = true;
     }
-    else if (event.key == 'ArrowUp' || event.key == 'w') {
+    else if (event.key == 'ArrowUp' || event.key == 'w' || event.key == 'W' || event.key == '8') {
         upPressed = true;
     }
-    else if (event.key == 'ArrowDown' || event.key == 's') {
+    else if (event.key == 'ArrowDown' || event.key == 's' || event.key == 'S' || event.key == '2') {
         downPressed = true;
     }
     move();
 }
 
-setInterval(function(){
+setInterval(function () {
     ctx.clearRect(0, 0, 1000, 500);
     if (bounce) {
-        if (obstacleY + 235 > 470) {
+        if (obstacleY > 465) {
             bounce = false;
         }
         obstacleY += Speed;
     }
     else {
-        if (obstacleY + 235 < 10) {
+        if (obstacleY < 10) {
             bounce = true;
         }
         obstacleY -= Speed;
@@ -59,16 +68,16 @@ setInterval(function(){
 }, 20);
 
 function stop(event) {
-    if (event.key == 'ArrowLeft' || event.key == 'a') {
+    if (event.key == 'ArrowLeft' || event.key == 'a' || event.key == 'A' || event.key == '4') {
         leftPressed = false;
     }
-    else if (event.key == 'ArrowRight' || event.key == 'd') {
+    else if (event.key == 'ArrowRight' || event.key == 'd' || event.key == 'D' || event.key == '6') {
         rightPressed = false;
     }
-    else if (event.key == 'ArrowUp' || event.key == 'w') {
+    else if (event.key == 'ArrowUp' || event.key == 'w' || event.key == 'W' || event.key == '8') {
         upPressed = false;
     }
-    else if (event.key == 'ArrowDown' || event.key == 's') {
+    else if (event.key == 'ArrowDown' || event.key == 's' || event.key == 'S' || event.key == '2') {
         downPressed = false;
     }
     move();
@@ -93,11 +102,12 @@ function move() {
 }
 
 function drawObstacle() {
-    for (let i = 4; i < 30; i++) {
+    for (let i = 0; i < 5; i++) {
         ctx.strokeStyle = 'black';
-        ctx.fillStyle = 'blue';
-        ctx.strokeRect(i * 30, obstacleY + 235, 20, 20);
-        ctx.fillRect(i * 30, obstacleY + 235, 20, 20);
+        ctx.fillStyle = 'blue'; 4
+        obstacleX[i] = (i + 4) * 100;
+        ctx.strokeRect(obstacleX[i], obstacleY, 20, 20);
+        ctx.fillRect(obstacleX[i], obstacleY, 20, 20);
     }
 }
 
