27ca05f3d24cb9770c7cfbf0b8548230c3f3a54c
ldh7228 <ldh7228@gmail.com>
2021-07-16 09:20:39 +0900

    Add main page

---
+++ src/main.js
@@ -0,0 +1,5 @@
+const playGame = document.getElementById('playGame');
+
+playGame.addEventListener('click', function() {
+    location.href = "file:///C:/Users/user/Desktop/World's%20Hardest%20Game/src/firstStage.html";
+})
