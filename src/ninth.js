4ba6a94cd439447b541b23a9e914af06c5d8d5ee
ldh7228 <ldh7228@gmail.com>
2021-07-21 18:53:56 +0900

    Add obstacle

--- src/ninth.js
+++ src/ninth.js
@@ -8,6 +8,8 @@ let leftPressed = false;
 let rightPressed = false;
 let upPressed = false;
 let downPressed = false;
+let heightBounce = true;
+let widthBounce = true;
 
 ctx.fillStyle = "black";
 ctx.fillRect(0, 0, 1000, 500)
@@ -16,34 +18,61 @@ ctx.lineWidth = 5;
 let playerX = 5;
 let playerY = 5;
 let playerSpeed = 2;
-let ObstacleSpeed = 10;
-let ObstacleX = [];
-let ObstacleY = 450;
-let num = 0;
-let num1 = 0;
+let heightObstacleSpeed = 3.5 + 1.75;
+let oddHeightObstacleX = [];
+let oddHeightObstacleY = 235;
+let evenHeightObstacleX = [];
+let evenHeightObstacleY = 235;
+let widthObstacleSpeed = 7.45 + 3.5 + 0.225;
+let oddWidthObstacleX = 500;
+let oddWidthObstacleY = [];
+let evenWidthObstacleX = 500;
+let evenWidthObstacleY = [];
+
+for (let num = 0; num < 10; num++) {
+}
 
 function drawPlayer() {
     if (playerX < 3) playerX = 3;
     if (playerY < 3) playerY = 3;
     if (playerX > 978) playerX = 978;
-    if (playerY > 478) playerY = 478;
+    if (playerY > 228) playerY = 228;
     for (let i = 0; i < 25; i++) {
-        for (j = 0; j < 721; j += 120) {
-            if ((ObstacleX[i] + 20) >= playerX && (ObstacleX[i] - 20) <= playerX && (ObstacleY - j) + 20 >= playerY && (ObstacleY - j) - 20 <= playerY) {
-                location.href = "gameover.html";
-            } // 게임오버 장면
-            if (playerX >= 900 && playerX <= 1000 && playerY >= 400 && playerY <= 500) {
-                location.href = "file:///C:/Users/user/Desktop/World's%20Hardest%20Game/src/fifthStage.html";
-            } // 다음 단계 이동
+        if ((oddHeightObstacleX[i] + 20) >= playerX && (oddHeightObstacleX[i] - 20) <= playerX && oddHeightObstacleY + 20 >= playerY && oddHeightObstacleY - 20 <= playerY) {
+            location.href = "gameover.html";
         }
+        if ((evenHeightObstacleX[i] + 20) >= playerX && (evenHeightObstacleX[i] - 20) <= playerX && evenHeightObstacleY + 20 >= playerY && evenHeightObstacleY - 20 <= playerY) {
+            location.href = "gameover.html";
+        }
+        if ((oddWidthObstacleX + 20) >= playerX && (oddWidthObstacleX - 20) <= playerX && oddWidthObstacleY[i] + 20 >= playerY && oddWidthObstacleY[i] - 20 <= playerY) {
+            location.href = "gameover.html";
+        }
+        if ((evenWidthObstacleX + 20) >= playerX && (evenWidthObstacleX - 20) <= playerX && evenWidthObstacleY[i] + 20 >= playerY && evenWidthObstacleY[i] - 20 <= playerY) {
+            location.href = "gameover.html";
+        }
+        if ((oddHeightObstacleX[i] + 20) >= playerX && (oddHeightObstacleX[i] - 20) <= playerX && oddHeightObstacleY + 20 >= playerY + 250 && oddHeightObstacleY - 20 <= playerY + 250) {
+            location.href = "gameover.html";
+        }
+        if ((evenHeightObstacleX[i] + 20) >= playerX && (evenHeightObstacleX[i] - 20) <= playerX && evenHeightObstacleY + 20 >= playerY + 250 && evenHeightObstacleY - 20 <= playerY + 250) {
+            location.href = "gameover.html";
+        }
+        if ((oddWidthObstacleX + 20) >= playerX && (oddWidthObstacleX - 20) <= playerX && oddWidthObstacleY[i] + 20 >= playerY + 250 && oddWidthObstacleY[i] - 20 <= playerY + 250) {
+            location.href = "gameover.html";
+        }
+        if ((evenWidthObstacleX + 20) >= playerX && (evenWidthObstacleX - 20) <= playerX && evenWidthObstacleY[i] + 20 >= playerY + 250 && evenWidthObstacleY[i] - 20 <= playerY + 250) {
+            location.href = "gameover.html";
+        }
+        // 게임오버 장면
+        if (playerX >= 900 && playerX <= 1000 && playerY >= 400 && playerY <= 500) {
+            location.href = "file:///C:/Users/user/Desktop/World's%20Hardest%20Game/src/seventhStage.html";
+        } // 다음 단계 이동
     }
     ctx.strokeStyle = 'black';
     ctx.fillStyle = 'red';
     ctx.strokeRect(playerX, playerY, 20, 20);
     ctx.fillRect(playerX, playerY, 20, 20);
     ctx.strokeRect(playerX, playerY + 250, 20, 20);
-    ctx.fillRect(playerX, playerY + 250, 20, 20);
-    // 플레이어 그리기
+    ctx.fillRect(playerX, playerY + 250, 20, 20); // 플레이어 그리기
 }
 
 function start(event) {
@@ -73,7 +102,7 @@ function stop(event) {
     }
     else if (event.key == 's' || event.key == 'S' || event.key == '2' || event.key == '5') {
         downPressed = false;
-    } //플레이어 스탑
+    }//플레이어 스탑
 }
 
 setInterval(function () {
@@ -81,25 +110,47 @@ setInterval(function () {
     ctx.fillStyle = "rgb(162, 255, 209)";
     ctx.fillRect(0, 0, 100, 100);
     ctx.fillRect(900, 400, 100, 100);
-    num++;
-    drawPlayer();
-    drawObstacleSub();
-    if (num > 20) {
-        ObstacleY -= ObstacleSpeed;
-        if (num > 39) {
-            num = 0;
-        }
+
+    if (oddHeightObstacleY > 465) {
+        heightBounce = false;
+    }
+    else if (oddHeightObstacleY < 10) {
+        heightBounce = true;
+    }
+    if (oddWidthObstacleX > 978) {
+        widthBounce = false;
+    }
+    else if (oddWidthObstacleX < 10) {
+        widthBounce = true;
+    }
+
+    if (heightBounce) {
+        oddHeightObstacleY += heightObstacleSpeed;
+        evenHeightObstacleY -= heightObstacleSpeed;
     }
     else {
-        ObstacleY += ObstacleSpeed;
+        oddHeightObstacleY -= heightObstacleSpeed;
+        evenHeightObstacleY += heightObstacleSpeed;
+    }
+    if (widthBounce) {
+        oddWidthObstacleX += widthObstacleSpeed;
+        evenWidthObstacleX -= widthObstacleSpeed;
+    }
+    else {
+        oddWidthObstacleX -= widthObstacleSpeed;
+        evenWidthObstacleX += widthObstacleSpeed;
     } // 장애물 이동
+
+    drawPlayer();
+    drawObstacle();
 }, 20);
 
 function move() {
     ctx.clearRect(0, 0, 1000, 500);
     ctx.fillStyle = "rgb(162, 255, 209)";
-    ctx.fillRect(0, 0, 100, 100)
-    ctx.fillRect(900, 400, 100, 100)
+    ctx.fillRect(0, 0, 100, 100);
+    ctx.fillRect(900, 400, 100, 100);
+
     if (leftPressed) {
         playerX -= playerSpeed;
     }
@@ -111,64 +162,50 @@ function move() {
     }
     if (downPressed) {
         playerY += playerSpeed;
-    } // 플레이어 이동
+    }
     drawPlayer();
-    drawObstacleSub();
+    drawObstacle();
     setTimeout(move, 10);
-}
+} // 플레이어 이동
 move();
 
-function drawObstacleSub() {
-    if (num1 == 0) {
-        ObstacleY -= 720;
-        num1++;
-        drawObstacleMain();
-    }
-    if (num1 == 1) {
-        ObstacleY += 120;
-        num1++;
-        drawObstacleMain();
-    }
-    if (num1 == 2) {
-        ObstacleY += 120;
-        num1++;
-        drawObstacleMain();
-    }
-    if (num1 == 3) {
-        ObstacleY += 120;
-        num1++;
-        drawObstacleMain();
-    }
-    if (num1 == 4) {
-        ObstacleY += 120;
-        num1++;
-        drawObstacleMain();
-    }
-    if (num1 == 5) {
-        ObstacleY += 120;
-        num1++;
-        drawObstacleMain();
-    }
-    if (num1 == 6) {
-        ObstacleY += 120;
-        num1 -= 6;
-        drawObstacleMain();
-    }
-} // 각 줄의 장애물 그리기
-function drawObstacleMain() {
-    for (let i = 0; i < 10; i++) {
+function drawObstacle() {
+    for (let i = 0; i < 18; i += 2) {
         ctx.strokeStyle = 'black';
         ctx.fillStyle = 'blue';
-        ObstacleX[i] = (i + 1.5) * 80;
-        ctx.strokeRect(ObstacleX[i], ObstacleY, 20, 20);
-        ctx.fillRect(ObstacleX[i], ObstacleY, 20, 20);
+        evenHeightObstacleX[i] = (i + 2.5) * 40;
+        ctx.strokeRect(evenHeightObstacleX[i], evenHeightObstacleY, 20, 20);
+        ctx.fillRect(evenHeightObstacleX[i], evenHeightObstacleY, 20, 20);
+    }
+    for (let i = 1; i < 18; i += 2) {
+        ctx.strokeStyle = 'black';
+        ctx.fillStyle = 'blue';
+        oddHeightObstacleX[i] = (i + 2.5) * 40;
+        ctx.strokeRect(oddHeightObstacleX[i], oddHeightObstacleY, 20, 20);
+        ctx.fillRect(oddHeightObstacleX[i], oddHeightObstacleY, 20, 20);
+    }
+    for (let i = 0; i < 4; i += 2) {
+        ctx.strokeStyle = 'black';
+        ctx.fillStyle = 'blue';
+        evenWidthObstacleY[i] = (i + 1.5) * 80;
+        ctx.strokeRect(evenWidthObstacleX, evenWidthObstacleY[i], 20, 20);
+        ctx.fillRect(evenWidthObstacleX, evenWidthObstacleY[i], 20, 20);
+    }
+    for (let i = 1; i < 4; i += 2) {
+        ctx.strokeStyle = 'black';
+        ctx.fillStyle = 'blue';
+        oddWidthObstacleY[i] = (i + 1.5) * 80;
+        ctx.strokeRect(oddWidthObstacleX, oddWidthObstacleY[i], 20, 20);
+        ctx.fillRect(oddWidthObstacleX, oddWidthObstacleY[i], 20, 20);
     } // 장애물 그리기
 }
+
 function reload(event) {
     if (event.key == 'r' || event.key == 'R') {
         location.reload();
-    } // 재시작
-}
+    }
+} // 재시작
+
 document.addEventListener('keydown', start);
 document.addEventListener('keyup', stop);
 document.addEventListener('keydown', reload);
