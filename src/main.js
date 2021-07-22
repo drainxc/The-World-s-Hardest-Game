4715008ee574e7fc6d4c9ae6b2e0586acdbe7f20
ldh7228 <ldh7228@gmail.com>
2021-07-22 09:41:12 +0900

    Add github event

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 const playGame = document.getElementById('playGame');
+const github = document.getElementById('github');
+
+github.addEventListener('click', function () {
+    location.href = "https://github.com/eastcopper";
+})
 
 playGame.addEventListener('click', function() {
     location.href = "file:///C:/Users/user/Desktop/World's%20Hardest%20Game/src/firstStage.html";
