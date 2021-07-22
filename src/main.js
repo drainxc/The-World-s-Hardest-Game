34b998c1f6d2e7e61276b041c9dc30f3e114ec16
ldh7228 <ldh7228@gmail.com>
2021-07-22 09:48:17 +0900

    Add tutorial

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 const playGame = document.getElementById('playGame');
 const github = document.getElementById('github');
+const tutorial = document.getElementById('tutorial');
 
 github.addEventListener('click', function () {
     location.href = "https://github.com/eastcopper";
-})
+}) // 깃허브 사이트 이동
 
 playGame.addEventListener('click', function() {
     location.href = "file:///C:/Users/user/Desktop/World's%20Hardest%20Game/src/firstStage.html";
 }) // 게임시작
+
+tutorial.addEventListener('click', function () {
+    location.href = "";
+})
