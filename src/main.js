ac9d8acd01e2d09cff827db5c2c5ebe351e95825
ldh7228 <ldh7228@gmail.com>
2021-07-23 11:41:56 +0900

    Add selectLevel click event

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 const playGame = document.getElementById('playGame');
 const github = document.getElementById('github');
 const tutorial = document.getElementById('tutorial');
+const selectLevel = document.getElementById('selectLevel');
+
+selectLevel.addEventListener('click', function() {
+    location.href = "file:///C:/Users/user/Desktop/World's%20Hardest%20Game/src/level.html";
+}) // 레벨 선택
 
 github.addEventListener('click', function () {
     location.href = "https://github.com/eastcopper";
